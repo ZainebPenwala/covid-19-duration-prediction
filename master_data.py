eb687f418fd2056a924b79232b4623259e4d866c
manas <manas.oid@gmail.com>
2020-03-28 19:06:49 +0530

    initial commit; adding the master data file for multi-valued features

---
+++ master_data.py
@@ -0,0 +1,33 @@
+CONTINENTS_MAPPING = {
+    'Asia' : 1,
+    'Europe' : 2,
+    'Africa' : 3,
+    'North America' : 4,
+    'South America' : 5,
+    'Antartica' : 6,
+    'Australia' : 7,
+}
+
+SYMPTOMS_MAPPING = {
+    'cough': 0, 
+    'respiratory symptoms': 1, 
+    'weakness': 2, 
+    'fever': 3, 
+    'nausea': 4, 
+    'vomiting': 5, 
+    'diarrhea': 6, 
+    'chills': 7, 
+    'body aches': 8, 
+    'sore throat': 9,
+}
+
+AGE_GROUPS_MAPPING = {
+    'children': 1,
+    'adults': 2,
+    'senior citizens': 3,
+}
+
+SYMPTOMS = ['cough', 'respiratory symptoms', 'weakness', 'fever', 'nausea', 'vomiting', 'diarrhea', 'chills', 
+    'body aches', 'sore throat']
+
+AGE_GROUPS = ['children', 'adults', 'senior citizens']
